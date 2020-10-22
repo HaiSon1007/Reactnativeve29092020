6ea2b925c7a32debab4351de68e717759f4a8115
phamtanphat <phatdroid94@gmail.com>
2020-10-22 18:48:31 +0700

    form ui

--- App.js
+++ App.js
@@ -2,11 +2,13 @@
 import React, {Component} from 'react';
 import {SafeAreaView} from 'react-native';
 import MainScreen from './src/screens/MainScreen';
+import Form from './src/components/Form';
 export default class App extends Component {
   render() {
     return (
       <SafeAreaView style={{flex: 1}}>
-        <MainScreen />
+        {/* <MainScreen /> */}
+        <Form />
       </SafeAreaView>
     );
   }
