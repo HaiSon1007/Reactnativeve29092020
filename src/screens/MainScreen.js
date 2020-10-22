19d5dda542f3d8feebac36f9085ca3c8ae4c1d73
phamtanphat <phatdroid94@gmail.com>
2020-10-22 18:36:28 +0700

    toggle and remove

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -5,17 +5,68 @@ import AppDimensions from '../utils/AppDimensions';
 import Box from '../components/Box';
 
 export default class MainScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: [
+        {id: 1, en: 'One', vn: 'Một', isMemorized: false},
+        {id: 2, en: 'Two', vn: 'Hai', isMemorized: false},
+        {id: 3, en: 'Three', vn: 'Ba', isMemorized: true},
+        {id: 4, en: 'Four', vn: 'Bon', isMemorized: true},
+        {id: 5, en: 'Five', vn: 'Nam', isMemorized: false},
+      ],
+    };
+  }
+  toggleWord = word => {
+    const newWords = this.state.words.map(item => {
+      if (item.id === word.id) {
+        return {...item, isMemorized: !item.isMemorized};
+      }
+      return item;
+    });
+    this.setState({words: newWords});
+  };
+  removeWord = word => {
+    const newWords = this.state.words.filter(item => {
+      if (item.id === word.id) {
+        return false;
+      }
+      return true;
+    });
+    this.setState({words: newWords});
+  };
   render() {
-    const words = [
-      {id: 1, en: 'One', vn: 'Một', isMemorized: false},
-      {id: 2, en: 'Two', vn: 'Hai', isMemorized: false},
-      {id: 3, en: 'Three', vn: 'Ba', isMemorized: true},
-      {id: 4, en: 'Four', vn: 'Bon', isMemorized: true},
-      {id: 5, en: 'Five', vn: 'Nam', isMemorized: false},
-    ];
     return (
       <View style={styles.container}>
-        <Box />
+        {this.state.words.map(word => {
+          return (
+            <View style={styles.containerWord} key={word.id.toString()}>
+              <View style={styles.containerText}>
+                <Text style={styles.textEn}>{word.en}</Text>
+                <Text style={styles.textVn}>
+                  {word.isMemorized ? '----' : word.vn}
+                </Text>
+              </View>
+              <View style={styles.containerTouchable}>
+                <TouchableOpacity
+                  onPress={() => this.toggleWord(word)}
+                  style={{
+                    ...styles.touchableMemorized,
+                    backgroundColor: word.isMemorized ? '#DD3444' : '#28a845',
+                  }}>
+                  <Text style={styles.textSize}>
+                    {word.isMemorized ? 'Memorized' : 'Forgot'}
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => this.removeWord(word)}
+                  style={styles.touchableRemove}>
+                  <Text style={styles.textSize}>Remove</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          );
+        })}
       </View>
     );
   }
@@ -64,32 +115,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFC106',
   },
 });
-
-/* 
-{words.map(word => {
-          return (
-            <View style={styles.containerWord} key={word.id.toString()}>
-              <View style={styles.containerText}>
-                <Text style={styles.textEn}>{word.en}</Text>
-                <Text style={styles.textVn}>
-                  {word.isMemorized ? '----' : word.vn}
-                </Text>
-              </View>
-              <View style={styles.containerTouchable}>
-                <TouchableOpacity
-                  style={{
-                    ...styles.touchableMemorized,
-                    backgroundColor: word.isMemorized ? '#DD3444' : '#28a845',
-                  }}>
-                  <Text style={styles.textSize}>
-                    {word.isMemorized ? 'Memorized' : 'Forgot'}
-                  </Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.touchableRemove}>
-                  <Text style={styles.textSize}>Remove</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          );
-        })}
-*/
