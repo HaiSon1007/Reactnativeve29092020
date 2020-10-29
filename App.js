04f19b79500e1d9f63387d1075a68729bfdedcc0
phamtanphat <phatdroid94@gmail.com>
2020-10-29 19:17:01 +0700

    setup redux

--- App.js
+++ App.js
@@ -6,6 +6,18 @@ import Form from './src/components/Form';
 import Filter from './src/components/Filter';
 import Box from './src/components/Box';
 import Word from './src/components/Word';
+import {createStore} from 'redux';
+
+const store = createStore(function(state = {count: 1}, action) {
+  if (action.type === 'INCREASE') {
+    return {count: state.count + 1};
+  }
+  return state;
+});
+
+console.log(store.getState());
+store.dispatch({type: 'INCREASE'});
+console.log(store.getState());
 
 export default class App extends Component {
   render() {
