909a1ec0d11bfdf0785c9724a3c7656f88d2539b
phamtanphat <phatdroid94@gmail.com>
2020-11-07 18:08:52 +0700

    map reducer to outside

--- App.js
+++ App.js
@@ -9,39 +9,7 @@ import Word from './src/components/Word';
 import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 
-const defaultWords = [
-  {id: 1, en: 'One', vn: 'Một', isMemorized: false},
-  {id: 2, en: 'Two', vn: 'Hai', isMemorized: false},
-  {id: 3, en: 'Three', vn: 'Ba', isMemorized: true},
-  {id: 4, en: 'Four', vn: 'Bon', isMemorized: true},
-  {id: 5, en: 'Five', vn: 'Nam', isMemorized: false},
-];
-function wordReducer(state = defaultWords, action) {
-  if (action.type === 'TOGGLE_WORD') {
-    const newWords = state.map(item => {
-      if (item.id === action.id) {
-        return {...item, isMemorized: !item.isMemorized};
-      }
-      return item;
-    });
-    return newWords;
-  }
-  if (action.type === 'REMOVE_WORD') {
-    const newWords = state.filter(item => {
-      if (item.id === action.id) {
-        return false;
-      }
-      return true;
-    });
-    return newWords;
-  }
-  if (action.type === 'ADD_WORD') {
-    const newWords = Object.assign([], state);
-    newWords.unshift(action.word);
-    return newWords;
-  }
-  return state;
-}
+
 function shouldShowFormReducer(state = false, action) {
   if (action.type === 'TOGGLE_FORM') {
     return !state;
